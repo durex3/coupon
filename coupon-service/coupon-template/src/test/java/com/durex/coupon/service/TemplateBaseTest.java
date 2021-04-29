3671e756de3a84e186e183256d0960198d7a54a1
liugelong <liugelong666@163.com>
2021-04-29 23:33:58 +0800

    优惠券模版基础服务测试用例

---
+++ coupon-service/coupon-template/src/test/java/com/durex/coupon/service/TemplateBaseTest.java
@@ -0,0 +1,47 @@
+package com.durex.coupon.service;
+
+import com.alibaba.fastjson.JSON;
+import com.durex.coupon.entity.CouponTemplate;
+import com.durex.coupon.vo.CouponTemplateSDK;
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author liugelong
+ * @date 2021/4/29 11:23 下午
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TemplateBaseTest {
+
+    @Resource
+    private ITemplateBaseService templateBaseService;
+
+    @Test
+    public void testGetById() {
+        CouponTemplate couponTemplate = templateBaseService.getById(13L);
+        System.out.println(JSON.toJSONString(couponTemplate));
+    }
+
+
+    @Test
+    public void testGetAllUsableTemplate() {
+        List<CouponTemplateSDK> allUsableTemplate = templateBaseService.getAllUsableTemplate();
+        System.out.println(JSON.toJSONString(allUsableTemplate));
+    }
+
+
+    @Test
+    public void testGetIds2TemplateSDK() {
+        Map<Long, CouponTemplateSDK> map = templateBaseService.getIds2TemplateSDK(Lists.newArrayList(13L));
+        System.out.println(JSON.toJSONString(map));
+    }
+
+}
